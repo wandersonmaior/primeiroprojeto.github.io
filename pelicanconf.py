deb58d3e7a7e8af0659e9c906cc15305d90f7245
viniciusaires7 <viniciusaires7@gmail.com>
2015-03-26 13:44:12 -0300

    Modificacao no icone do Coding Dojo

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ AUTHOR = u'DojoTO'
 AUTHOR_EMAIL = u'gravatar@dojoto.info'
 SITENAME = u'Coding Dojo Tocantins'
 SITEURL = 'http://dojoto.info'
-SITELOGO = 'images/logo-dojoto.svg'
+SITELOGO = 'images/dojo-white.png'
 
 GITHUB_USERNAME = 'dojoto'
 GITHUB_PROJECT = 'dojoto.github.io'
